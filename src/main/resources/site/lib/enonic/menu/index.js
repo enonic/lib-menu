8a71b229b4e5df92ea9d1aa2b9b6f1e95891f672
runarmyklebust <rmy@enonic.com>
2018-01-16 12:42:19 +0100

    Increase getMenuTree performance #21
    Upped to v2.0.0

--- src/main/resources/site/lib/enonic/menu/index.js
+++ src/main/resources/site/lib/enonic/menu/index.js
@@ -5,7 +5,7 @@ var libs = {
 };
 
 var globals = {
-	appPath: libs.util.app.getJsonName()
+    appPath: libs.util.app.getJsonName()
 };
 
 /**
@@ -109,7 +109,7 @@ exports.getMenuTree = function(levels) {
     levels = (libs.util.value.isInt(levels) ? levels : 1);
 
     if (site) {
-        menu = exports.getSubMenus(site, levels);
+        menu = doGetSubMenus(site, levels);
     }
 
     return menu;
@@ -121,37 +121,63 @@ exports.getMenuTree = function(levels) {
  * @param {Integer} levels - The number of submenus to retrieve
  * @return {Array} Array of submenus
  */
-exports.getSubMenus = function(parentContent, levels) {
+exports.getSubMenus = function (parentContent, levels) {
+    return doGetSubMenus(parentContent, levels);
+};
+
+var doGetSubMenus = function (parentContent, levels) {
     var subMenus = [];
+    var currentContent = libs.portal.getContent();
+
 
     if (parentContent.type === 'portal:site' && isMenuItem(parentContent)) {
-        subMenus.push(menuItemToJson(parentContent, 0));
+        subMenus.push(renderMenuItem(currentContent, parentContent, 0));
     }
 
-    var children = libs.content.getChildren({
-        key: parentContent._id,
-        count: 200
-    });
+    var children = getChildMenuItems(parentContent);
 
     levels--;
 
-	var loopLength = children.hits.length;
-	for (var i = 0; i < loopLength; i++) {
-		var child = children.hits[i];
-		if (isMenuItem(child)) {
-			subMenus.push(menuItemToJson(child, levels));
-		}
-	}
+    var loopLength = children.hits.length;
+    for (var i = 0; i < loopLength; i++) {
+        var child = children.hits[i];
+        if (isMenuItem(child)) {
+            subMenus.push(renderMenuItem(currentContent, child, levels));
+        }
+    }
 
-	return subMenus;
+    return subMenus;
 };
 
+var getChildMenuItems = function (parent) {
+    return libs.content.query({
+        count: 1000,
+        filters: {
+            boolean: {
+                must: [
+                    {
+                        hasValue: {
+                            field: "_parentPath",
+                            values: [
+                                "/content" + parent._path
+                            ]
+                        }
+                    }
+                    ,
+                    {
+                        hasValue: {
+                            field: "x." + globals.appPath + ".menu-item.menuItem",
+                            values: [
+                                "true"
+                            ]
+                        }
+                    }
+                ]
+            }
+        }
+    });
+};
 
-/**
- * Checks if the content is a menu item.
- * @param {Content} content - content object obtained with 'portal.getContent', 'portal.getSite' or any 'content.*' commands
- * @return {Boolean} true if the content is marked as menu item
- */
 function isMenuItem(content) {
     var extraData = content.x;
     if (!extraData) {
@@ -166,33 +192,25 @@ function isMenuItem(content) {
     return menuItemMetadata['menuItem'];
 }
 
-/**
- * Returns JSON data for a menuitem.
- * @param {Content} content - content object obtained with 'portal.getContent', 'portal.getSite' or any 'content.*' commands
- * @param {Integer} levels - The number of submenus to retrieve
- * @return {Object} Menuitem JSON data
- */
-function menuItemToJson(content, levels) {
+function renderMenuItem(currentContent, content, levels) {
     var subMenus = [];
     if (levels > 0) {
-        subMenus = exports.getSubMenus(content, levels);
+        subMenus = doGetSubMenus(content, levels);
     }
 
-	 var inPath = false;
-	 var isActive = false;
+    var inPath = false;
+    var isActive = false;
 
-	 var currentContent = libs.portal.getContent();
+    // Is the menuitem we are processing in the currently viewed content's path?
+    if (content._path === currentContent._path.substring(0, content._path.length)) {
+        inPath = true;
+    }
 
-	 // Is the menuitem we are processing in the currently viewed content's path?
-	 if ( content._path === currentContent._path.substring(0,content._path.length) ) {
-		 inPath = true;
-	 }
-
-	 // Is the currently viewed content the current menuitem we are processing?
-	 if ( content._path === currentContent._path ) {
-		 isActive = true;
-		 inPath = false; // Reset this so a menuitem isn't both in a path and active (makes no sense)
-	 }
+    // Is the currently viewed content the current menuitem we are processing?
+    if (content._path === currentContent._path) {
+        isActive = true;
+        inPath = false; // Reset this so an menuitem isn't both in a path and active (makes no sense)
+    }
 
     var menuItem = content.x[globals.appPath]['menu-item'];
 
